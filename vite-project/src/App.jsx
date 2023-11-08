3abf458f59cbba90625456391fae4607feaeec2a
Tyler_Williams125 <89519395+tylerwilliams125@users.noreply.github.com>
2023-11-08 09:06:49 -0600

    ToDoList

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'boostrap/dist/js/bootstrap.min.js'
+import './App.css'
+import ToDoList from './components/ToDoList'
+
+function App() {
+  
+  return (
+    <>
+      <ToDoList />
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/components/ToDoList.jsx
@@ -0,0 +1,10 @@
+export default function ToDoList() {
+    return (
+        <>
+        <h1>To Do List App</h1>
+        </>
+    )
+
+
+
+}
